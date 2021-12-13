90c781e70ce12189a54ce7dab28c3e56b34ed592
Mihai Stanciu <mstanciu552@gmail.com>
2021-12-13 14:37:17 +0200

    (change) refactored database code OOP

--- get_connection.py
+++ get_connection.py
@@ -1,29 +1,20 @@
-import migrations.db_conn as db
-import sqlite3
-from sqlite3 import Error
-path = "migrations/db/myGit.sqlite"
+from migrations.db_conn import Database
 from github import Github
 from github import GithubException
 #the get_token() function receive the user_id parameter and it returns the GitHub Token
 
-def get_connection(user_id):
-    connection = db.create_connection(path)
-    cursor = connection.cursor()
-    query = """
-        SELECT * FROM users
-        WHERE id = """ + str(user_id) + """;
-    """
-    cursor.execute(query)
-    rows = cursor.fetchall()
+def get_connection(db, user_id):
+    rows = db.select('users', user_id)
 
     if rows == []:
         return -1
     
-    #test the result/verify if it connects to Github
-   
+    # test the result/verify if it connects to Github
     try:
         git = Github(rows[0][1])
         git.get_repo(1)
         return git
     except GithubException as e:
         return -1
+
+
